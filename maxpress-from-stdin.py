4c99e73374948417505bd6e8a1203a73398d0d40
muxueqz <zhangmingyuan240@gmail.com>
2021-12-16 16:02:54 +0800

    bugfix. 修复有时微信服务器获取不到图片的问题

--- maxpress-from-stdin.py
+++ maxpress-from-stdin.py
@@ -1,3 +1,4 @@
+from urllib.request import urlopen, Request
 import sys, os, re
 from os.path import join as join_path
 import base64
@@ -5,6 +6,20 @@ from maxpress import import_config, compile_styles, md2html
 
 ROOT = os.path.dirname(sys.argv[0])
 
+def img2base64(html):    # 修复有时微信服务器获取不到图片的问题
+    imgs = re.findall('img src="([^"]*)', html)
+
+    headers = { 'User-Agent' : 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.93 Safari/537.36' }
+    new_imgs = {}
+
+    for i in imgs:
+        req = Request(i, headers=headers)
+        image_data = urlopen(req).read()
+        encoded = base64.b64encode(image_data) # Creates a bytes object
+        img_path = 'data:image/png;base64,{}'.format(encoded.decode('utf8'))
+        html = html.replace(i, img_path)
+
+    return html
 
 # 转换temp下的所有md文档
 # @report_error
@@ -25,7 +40,7 @@ def convert_all(src=join_path(ROOT, 'temp'),
                      banner=config['banner_url'],
                      convert_list=config['convert_list'],
                      ul_style=config['ul_style'])
-    print(result)
+    print(img2base64(result))
 
 if __name__ == '__main__':
 
